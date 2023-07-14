db9e458d1e6158d8fddb1f038bcf20bba8a86d72
tokyolem <heenworker@gmail.com>
2023-07-14 20:50:32 +0300

    Initial commit

---
+++ avtovas_mobile/android/app/src/main/kotlin/com/avtovas/appavtovas/avtovas_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.avtovas.appavtovas.avtovas_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ common/example/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppExample());
+}
+
+final class AppExample extends StatelessWidget {
+  const AppExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
