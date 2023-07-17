bac580747bc4a1fb553aa5cadb314f1d7740185f
softtrack <glebdyakov2000@gmail.com>
2023-07-17 18:51:42 +0300

    Search ui: display filters btn

--- avtovas_mobile/lib/main.dart
+++ avtovas_mobile/lib/main.dart
@@ -188,6 +188,36 @@ class _MyHomePageState extends State<MyHomePage> {
                             )
                           )
                         ),
+                        Container(
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                maximumSize: MaterialStateProperty.all<Size>(
+                                  Size(115.0, 33.0)
+                                ),
+                                minimumSize: MaterialStateProperty.all<Size>(
+                                  Size(105.0, 33.0)
+                                ),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  )
+                                ),
+                                backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.white
+                                ),
+                                foregroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.black
+                                )
+                            ),
+                            onPressed: () {
+                              // TODO
+                            },
+                            child: Text('Фильтры')
+                          ),
+                          margin: EdgeInsets.only(
+                            left: 9
+                          )
+                        )
                       ]
                     ),
                     margin: EdgeInsets.only(
