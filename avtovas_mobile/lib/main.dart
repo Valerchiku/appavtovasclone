e3606cc3cdb2c2eec1c13d0422dca4f792521ad1
softtrack <glebdyakov2000@gmail.com>
2023-07-17 18:45:50 +0300

    Search ui: display from selector

--- avtovas_mobile/lib/main.dart
+++ avtovas_mobile/lib/main.dart
@@ -71,6 +71,45 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Colors.white,
                       fontSize: 30
                     )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 39
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(7),
+                      border: Border.all(
+                        width: 2,
+                        color: Color(0xFF2E74AF)
+                      )
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 6,
+                      vertical: 7
+                    ),
+                    child:  DropdownButton<String>(
+                      value: from,
+                      icon: const Icon(
+                        Icons.arrow_drop_down
+                      ),
+                      isExpanded: true,
+                      underline: Container(
+                        height: 2,
+                        color: Colors.transparent
+                      ),
+                      onChanged: (String? newValue) {
+                        // TODO
+                      },
+                      items: routes.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()
+                    ),
+                    width: 335,
+                    height: 45
                   )
                 ]
               )
