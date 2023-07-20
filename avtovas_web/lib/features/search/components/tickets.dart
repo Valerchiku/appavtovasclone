264d21463b9dcc55373c9cd9ec545193df17c5ce
softtrack <glebdyakov2000@gmail.com>
2023-07-20 13:51:19 +0300

    Search ui: reuse calendar for web

--- avtovas_web/lib/features/search/components/tickets.dart
+++ avtovas_web/lib/features/search/components/tickets.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
-import '../../../utils/utils.dart';
-
+import 'package:common/avtovas_common.dart';
 
 class SearchTicketsWidget extends StatefulWidget {
   const SearchTicketsWidget({super.key});
