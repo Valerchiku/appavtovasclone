d230a31964decc0dd70dd03ab35ce8100a18f4f2
tokyolem <heenworker@gmail.com>
2023-07-21 14:22:34 +0300

    Bottom nav bar: add bottom navigation bar, update localization arb

---
+++ avtovas_mobile/lib/src/common/constants/app_dimensions.dart
@@ -0,0 +1,11 @@
+abstract final class AppDimensions {
+  static const double none = 0;
+  static const double extraSmall = 2;
+  static const double small = 4;
+  static const double medium = 8;
+  static const double large = 16;
+  static const double extraLarge = 32;
+
+  static const double navigationPanelHeight = 80;
+  static const double navigationPanelIconSize = 20;
+}
