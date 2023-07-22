eaeae60b7fdc0033fea3012b16f636844c8b9349
Tash <tasm86688@gmail.com>
2023-07-22 11:13:41 +0500

    Searchable menu: fixed localizations issues

--- common/lib/src/localization/flutter_gen/avtovas_localizations.dart
+++ common/lib/src/localization/flutter_gen/avtovas_localizations.dart
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
@@ -21,8 +13,6 @@ import 'avtovas_localizations_ru.dart';
 // ignore_for_file: public_member_api_docs
 // ignore_for_file: noop_primitive_operations
 // ignore_for_file: member-ordering
-
-
 
 /// Callers can lookup localized strings with an instance of AvtovasLocalization
 /// returned by `AvtovasLocalization.of(context)`.
@@ -76,7 +66,8 @@ import 'avtovas_localizations_ru.dart';
 /// be consistent with the languages listed in the AvtovasLocalization.supportedLocales
 /// property.
 abstract class AvtovasLocalization {
-  AvtovasLocalization(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  AvtovasLocalization(String locale)
+      : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
@@ -84,7 +75,8 @@ abstract class AvtovasLocalization {
     return Localizations.of<AvtovasLocalization>(context, AvtovasLocalization)!;
   }
 
-  static const LocalizationsDelegate<AvtovasLocalization> delegate = _AvtovasLocalizationDelegate();
+  static const LocalizationsDelegate<AvtovasLocalization> delegate =
+      _AvtovasLocalizationDelegate();
 
   /// A list of this localizations delegate along with the default localizations
   /// delegates.
@@ -96,7 +88,8 @@ abstract class AvtovasLocalization {
   /// Additional delegates can be added by appending to this list in
   /// MaterialApp. This list does not have to be used at all if a custom list
   /// of delegates is preferred or required.
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
     delegate,
     GlobalMaterialLocalizations.delegate,
     GlobalCupertinoLocalizations.delegate,
@@ -157,33 +150,29 @@ abstract class AvtovasLocalization {
   String get wherever;
 }
 
-class _AvtovasLocalizationDelegate extends LocalizationsDelegate<AvtovasLocalization> {
+class _AvtovasLocalizationDelegate
+    extends LocalizationsDelegate<AvtovasLocalization> {
   const _AvtovasLocalizationDelegate();
 
   @override
   Future<AvtovasLocalization> load(Locale locale) {
-    return SynchronousFuture<AvtovasLocalization>(lookupAvtovasLocalization(locale));
+    return SynchronousFuture<AvtovasLocalization>(
+        lookupAvtovasLocalization(locale));
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['ru'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => true;
 
   @override
   bool shouldReload(_AvtovasLocalizationDelegate old) => false;
 }
 
 AvtovasLocalization lookupAvtovasLocalization(Locale locale) {
-
-
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
-    case 'ru': return AvtovasLocalizationRu();
+    case 'ru':
+      return AvtovasLocalizationRu();
+    default:
+      return AvtovasLocalizationRu();
   }
-
-  throw FlutterError(
-    'AvtovasLocalization.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.',
-  );
 }
