4b234505c537c79e4c6a07a4189e10338c72b66f
softtrack <glebdyakov2000@gmail.com>
2023-07-23 10:40:35 +0300

    Help info ui: add help info page root widget

--- avtovas_mobile/lib/src/features/help_info/pages/help_info.dart
+++ avtovas_mobile/lib/src/features/help_info/pages/help_info.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HelpInfoPage extends StatelessWidget {
   const HelpInfoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      body: null,
+    );
   }
 }
