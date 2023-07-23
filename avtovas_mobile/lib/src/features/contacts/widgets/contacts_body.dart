7175814b80684ed693e4cb25769a153c3df6bdf9
softtrack <glebdyakov2000@gmail.com>
2023-07-23 10:18:32 +0300

    Contacts ui: add other margins

--- avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
+++ avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
@@ -27,68 +27,93 @@ class ContactsPageBody extends StatelessWidget {
                 fontWeight: FontWeight.w400,
               ),
             ),
-            Text(
-              '+7 (8352) 28-90-00',
-              style: TextStyle(
-                color: ContactsColors.Colors.phone,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              '${context.locale.from} 05:00 ${context.locale.to} 20:00 ${context.locale.daily}',
-              style: TextStyle(
-                color: ContactsColors.Colors.label,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              '${context.locale.control_room_of_the_central_bus_station}',
-              style: TextStyle(
-                color: ContactsColors.Colors.label,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              '+7 (8352) 28-90-00',
-              style: TextStyle(
-                color: ContactsColors.Colors.phone,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              '${context.locale.support}',
-              style: TextStyle(
-                color: ContactsColors.Colors.phone,
-                fontSize: Dimensions.titleFont,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '8 (800) 700-02-40',
-              style: TextStyle(
-                color: ContactsColors.Colors.phone,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              context.locale.round_the_clock,
-              style: TextStyle(
-                color: ContactsColors.Colors.label,
-                fontSize: Dimensions.labelFont,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            Text(
-              '${context.locale.contacts}',
-              style: TextStyle(
-                color: ContactsColors.Colors.phone,
-                fontSize: Dimensions.titleFont,
-                fontWeight: FontWeight.w700,
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemLabelMarginTop),
+                child: Text(
+                  '+7 (8352) 28-90-00',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.phone,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemLabelMarginTop),
+                child: Text(
+                  '${context.locale.from} 05:00 ${context.locale.to} 20:00 ${context.locale.daily}',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.label,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemTitleMarginTop),
+                child: Text(
+                  '${context.locale.control_room_of_the_central_bus_station}',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.label,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemLabelMarginTop),
+                child: Text(
+                  '+7 (8352) 28-90-00',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.phone,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemTitleMarginTop),
+                child: Text(
+                  '${context.locale.support}',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.phone,
+                    fontSize: Dimensions.titleFont,
+                    fontWeight: FontWeight.w700,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemTitleMarginTop),
+                child: Text(
+                  '8 (800) 700-02-40',
+                  style: TextStyle(
+                    color: ContactsColors.Colors.phone,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: Dimensions.contactsItemLabelMarginTop),
+                child: Text(
+                  context.locale.round_the_clock,
+                  style: TextStyle(
+                    color: ContactsColors.Colors.label,
+                    fontSize: Dimensions.labelFont,
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+            Padding(
+              padding:
+                  EdgeInsets.only(top: Dimensions.contactsItemTitleMarginTop),
+              child: Text(
+                '${context.locale.contacts}',
+                style: TextStyle(
+                  color: ContactsColors.Colors.phone,
+                  fontSize: Dimensions.titleFont,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
             ContactsItem(),
