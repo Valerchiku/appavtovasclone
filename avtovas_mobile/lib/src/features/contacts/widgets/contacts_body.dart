a7e9b6e858057c6b09a5fd64bb42231deb98e850
softtrack <glebdyakov2000@gmail.com>
2023-07-23 09:10:12 +0300

    Contacts ui: add root padding

--- avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
+++ avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:avtovas_mobile/src/features/contacts/utils/constants/dimensions.dart';
 
 class ContactsPageBody extends StatelessWidget {
   const ContactsPageBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Padding(
+      padding: EdgeInsets.only(
+          left: Dimensions.contactsPaddingLeft,
+          top: Dimensions.contactsPaddingTop),
+      child: Placeholder(),
+    );
   }
 }
