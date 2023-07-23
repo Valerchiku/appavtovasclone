06436223b5d63c57a491d5cd7a5d1d4848e492d7
softtrack <glebdyakov2000@gmail.com>
2023-07-23 09:39:19 +0300

    Contacts ui: add label to body

--- avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
+++ avtovas_mobile/lib/src/features/contacts/widgets/contacts_body.dart
@@ -1,6 +1,9 @@
+import 'package:common/avtovas_common.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:avtovas_mobile/src/features/contacts/utils/constants/dimensions.dart';
+import 'package:avtovas_mobile/src/features/contacts/utils/constants/colors.dart'
+    as ContactsColors;
 
 class ContactsPageBody extends StatelessWidget {
   const ContactsPageBody({super.key});
@@ -8,10 +11,20 @@ class ContactsPageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(
-          left: Dimensions.contactsPaddingLeft,
-          top: Dimensions.contactsPaddingTop),
-      child: Placeholder(),
-    );
+        padding: EdgeInsets.only(
+            left: Dimensions.contactsPaddingLeft,
+            top: Dimensions.contactsPaddingTop),
+        child: Column(
+          children: [
+            Text(
+              context.locale.info_desk_of_the_central_bus_station,
+              style: TextStyle(
+                color: ContactsColors.Colors.label,
+                fontSize: Dimensions.labelFont,
+                fontWeight: FontWeight.w400,
+              ),
+            )
+          ],
+        ));
   }
 }
