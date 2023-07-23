ba8bdcdc591365075531018db1301137cfe87fc8
softtrack <glebdyakov2000@gmail.com>
2023-07-23 09:52:29 +0300

    Contacts ui: add title font size dimension

--- avtovas_mobile/lib/src/features/contacts/utils/constants/dimensions.dart
+++ avtovas_mobile/lib/src/features/contacts/utils/constants/dimensions.dart
@@ -2,4 +2,5 @@ abstract final class Dimensions {
   static const double contactsPaddingLeft = 20;
   static const double contactsPaddingTop = 21;
   static const double labelFont = 14;
+  static const double titleFont = 18;
 }
