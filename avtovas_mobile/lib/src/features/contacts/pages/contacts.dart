be56ad209342f04f4ee0bc45923a0e9e3fd0ffa6
softtrack <glebdyakov2000@gmail.com>
2023-07-23 09:03:05 +0300

    Contacts ui: add contacts page body

--- avtovas_mobile/lib/src/features/contacts/pages/contacts.dart
+++ avtovas_mobile/lib/src/features/contacts/pages/contacts.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/contacts_body.dart';
+
 class ContactsPage extends StatelessWidget {
   const ContactsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: null,
+      body: ContactsPageBody(),
     );
   }
 }
