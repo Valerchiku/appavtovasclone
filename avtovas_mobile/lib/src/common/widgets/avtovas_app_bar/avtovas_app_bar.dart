5e19a4ce20620721b94fa1f2e45c9256da134ea6
Muhammed Tash <119746836+M26Tash@users.noreply.github.com>
2023-07-24 14:07:09 +0500

    Help ui: fixed avtovas app bar

--- avtovas_mobile/lib/src/common/widgets/avtovas_app_bar/avtovas_app_bar.dart
+++ avtovas_mobile/lib/src/common/widgets/avtovas_app_bar/avtovas_app_bar.dart
@@ -32,7 +32,6 @@ class AvtovasAppBar extends StatelessWidget implements PreferredSizeWidget {
           fontSize: CommonFonts.appBarFontSize,
         ),
       ),
-      centerTitle: false,
     );
   }
 
