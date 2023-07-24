7d996a424d8ccb787160633381aeafca0b0074e3
Tash <tasm86688@gmail.com>
2023-07-24 16:14:56 +0500

    Avtovas app bar: added app bar

---
+++ avtovas_mobile/lib/src/common/widgets/avtovas_app_bar/avtovas_app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:common/avtovas_common.dart';
+import 'package:common/src/utils/constants/common_fonts.dart';
+import 'package:flutter/material.dart';
+
+// ignore_for_file: implementation_imports
+
+class AvtovasAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final String svgAssetPath;
+  final VoidCallback onTap;
+
+  const AvtovasAppBar({
+    required this.title,
+    required this.svgAssetPath,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: context.theme.mainAppColor,
+      leading: AvtovasVectorButton(
+        onTap: onTap,
+        svgAssetPath: svgAssetPath,
+      ),
+      title: Text(
+        title,
+        style: context.themeData.textTheme.displaySmall?.copyWith(
+          color: context.theme.whitespaceContainerColor,
+          fontWeight: CommonFonts.weightRegular,
+          fontSize: CommonFonts.appBarFontSize,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
