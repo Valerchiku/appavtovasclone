d2edf85e6b36ecc13003e1e4d1804ee00559b61d
softtrack <glebdyakov2000@gmail.com>
2023-07-26 08:39:08 +0300

    Search ui 2: add common constants

--- common/lib/src/utils/constants/common_dimensions.dart
+++ common/lib/src/utils/constants/common_dimensions.dart
@@ -22,6 +22,35 @@ abstract final class CommonDimensions {
   static const double datePickerBtnWidth = 105;
 
   static const double datePickerBtnHeight = 33;
+
+  static const double selectorHeight = 45;
+  static const double dateBtnLabelMarginLeft = 13.81;
+  static const double logoWidth = 270.25;
+  static const double logoHeight = 138.38;
+  static const double recentTripPaddingLeft = 14;
+  static const double recentTripPaddingTop = 6;
+  static const double recentTripWidth = 162;
+  static const double recentTripHeight = 48;
+  static const double fromTripSelectorMarginTop = 39;
+  static const double toggleBtnBorder = 1;
+  static const double selectorMarginTop = 10;
+  static const double selectorRadius = 5;
+  static const double titleSize = 30;
+  static const double selectorPaddingLeft = 12;
+  static const double selectorPaddingVertical = 15;
+  static const double clearHistoryLabelMarginTop = 16;
+  static const double recentItemsMarginTop = 18;
+  static const double recentMarginTop = 42;
+  static const double recentIconMarginLeft = 15;
+  static const double rootPaddingHorizontal = 29;
+  static const double rootPaddingTop = 79;
+  static const double rootPaddingBottom = 30;
+  static const double dateFilterMarginTop = 10;
+  static const double recentTripMarginLeft = 5;
+  static const double recentTripRadius = 9;
+  static const BorderRadius selectorBorderRadius =
+      BorderRadius.all(Radius.circular(selectorRadius));
+
 }
 
 extension AvailableSize on BuildContext {
