887cc4b13d3f4d9b86ed267cd074f39b8a862667
softtrack <glebdyakov2000@gmail.com>
2023-07-26 18:23:16 +0300

    Passengers ui: add item widget

--- avtovas_mobile/lib/src/features/passengers/widgets/item.dart
+++ avtovas_mobile/lib/src/features/passengers/widgets/item.dart
@@ -7,34 +7,68 @@ import 'package:avtovas_mobile/src/features/passengers/utils/constants/colors.da
 as HelpInfoColors;
 
 class Item extends StatelessWidget {
-  final String title;
-  final String content;
+  final String name;
+  final String age;
+  final String gender;
   final Function onSelected;
 
   const Item(
-      {required this.title,
-        required this.content,
+      {required this.name,
+        required this.age,
+        required this.gender,
         required this.onSelected,
         super.key});
 
+  getAgeAndGender () {
+    var result = '';
+    int ageLen = age.length;
+    int genderLen = gender.length;
+    if (ageLen > 0 && genderLen > 0) {
+      result = '${age}, ${gender}';
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: Dimensions.itemMarginTop),
-      child: Column(children: [
-        ExpansionTile(
-            shape: Border(),
-            onExpansionChanged: (val) {
-              onSelected();
-            },
-            trailing: AvtovasVectorImage(svgAssetPath: ImagesAssets.closedItem),
-            title: Text(title),
-            children: <Widget>[Text(content)],
-            initiallyExpanded: false),
-        Divider(
-          color: HelpInfoColors.Colors.divider,
-        )
-      ]),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      right: 13
+                  ),
+                  child: Image.asset(ImagesAssets.somePassengerPlate),
+                ),
+                Column(
+                  children: [
+                    Text(
+                        name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                    ),
+                    Text(
+                      getAgeAndGender(),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            AvtovasVectorImage(svgAssetPath: ImagesAssets.chevron)
+          ],
+        ),
+        Divider()
+      ],
     );
   }
 }
--- avtovas_mobile/lib/src/features/passengers/widgets/passengers_screen_body.dart
+++ avtovas_mobile/lib/src/features/passengers/widgets/passengers_screen_body.dart
@@ -1,5 +1,3 @@
-import 'package:avtovas_mobile/src/features/app/pages/app.dart';
-import 'package:avtovas_mobile/src/features/passengers/widgets/item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
