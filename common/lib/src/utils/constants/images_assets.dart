71ccbb160c05a6733f54f5a3b32fe8a615d7ac91
softtrack <glebdyakov2000@gmail.com>
2023-07-26 14:57:34 +0300

    Passengers ui: add image assets

--- common/lib/src/utils/constants/images_assets.dart
+++ common/lib/src/utils/constants/images_assets.dart
@@ -1,7 +1,11 @@
 abstract final class ImagesAssets {
   static const String busIcon = '${_assetsVector}bus_icon.svg';
   static const String closedItem = '${_assetsVector}closed_item.svg';
+  static const String passenger = '${_assetsVector}passenger.svg';
+  static const String chevron = '${_assetsVector}chevron.svg';
+  static const String somePassengerPlate = '${_assetsImages}some_passenger_plate.png';
 
   static const String _assetsFolder = 'assets/';
   static const String _assetsVector = '${_assetsFolder}vector/';
+  static const String _assetsImages = '${_assetsFolder}images/';
 }
