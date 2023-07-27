25895e442ddcbf4db61c3e483912ee6e5023abed
tokyolem <heenworker@gmail.com>
2023-07-28 00:00:15 +0300

    Expansion container: change expansion container behaivor

--- common/lib/src/widgets/utils_widgets/expansion_container.dart
+++ common/lib/src/widgets/utils_widgets/expansion_container.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:common/src/utils/constants/common_dimensions.dart';
 import 'package:common/src/utils/list_extension.dart';
 import 'package:flutter/material.dart';
@@ -6,10 +8,13 @@ class ExpansionContainer extends StatefulWidget {
   final Widget title;
   final List<Widget> children;
   final ValueChanged<bool>? onStatusChanged;
+  final bool showArrow;
+  final ArrowAlignment arrowAlignment;
   final double? sizeBetweenChildren;
   final Color? backgroundColor;
   final EdgeInsets? margin;
   final EdgeInsets? padding;
+  final EdgeInsets? titlePadding;
   final Border? border;
   final BorderRadius? borderRadius;
 
@@ -17,10 +22,13 @@ class ExpansionContainer extends StatefulWidget {
     required this.title,
     required this.children,
     this.onStatusChanged,
+    this.showArrow = true,
+    this.arrowAlignment = ArrowAlignment.start,
     this.sizeBetweenChildren,
     this.backgroundColor,
     this.margin,
     this.padding,
+    this.titlePadding,
     this.border,
     this.borderRadius,
     super.key,
@@ -31,52 +39,84 @@ class ExpansionContainer extends StatefulWidget {
 }
 
 class _ExpansionContainerState extends State<ExpansionContainer>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _animation;
-  bool _isExpanded = false;
+    with TickerProviderStateMixin {
+  late final AnimationController _expansionController;
+  late final Animation<double> _expansionAnimation;
+  late final AnimationController? _rotationController;
+  late final Animation<double>? _rotationAnimation;
+
+  var _isExpanded = false;
 
   @override
   void initState() {
     super.initState();
 
-    _animationController = AnimationController(
+    _expansionController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 200),
     );
 
-    _animation = CurvedAnimation(
-      parent: _animationController,
+    _expansionAnimation = CurvedAnimation(
+      parent: _expansionController,
       curve: Curves.fastOutSlowIn,
     );
+
+    if (widget.showArrow) {
+      _rotationController = AnimationController(
+        vsync: this,
+        duration: const Duration(milliseconds: 200),
+      );
+
+      const beginRotation = 0.0;
+      const endRotation = 0.5;
+
+      _rotationAnimation = Tween(
+        begin: beginRotation,
+        end: endRotation,
+      ).animate(
+        CurvedAnimation(
+          parent: _rotationController!,
+          curve: Curves.fastOutSlowIn,
+        ),
+      );
+    }
   }
 
   void _onContainerTap() {
     setState(() => _isExpanded = !_isExpanded);
-    _isExpanded
-        ? _animationController.forward()
-        : _animationController.reverse();
+
+    if (_isExpanded) {
+      _expansionController.forward();
+      _rotationController?.forward();
+    } else {
+      _expansionController.reverse();
+      _rotationController?.reverse();
+    }
+
     widget.onStatusChanged?.call(_isExpanded);
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _expansionController.dispose();
+    _rotationController?.dispose();
 
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    const rotationCount = 2;
+
     return InkWell(
       onTap: _onContainerTap,
-      borderRadius: const BorderRadius.only(
-        bottomLeft: Radius.circular(CommonDimensions.large),
-        bottomRight: Radius.circular(CommonDimensions.large),
-      ),
+      borderRadius: widget.borderRadius,
       child: Container(
-        margin: widget.margin,
-        padding: widget.padding,
+        margin: widget.margin ?? EdgeInsets.zero,
+        padding: widget.padding ??
+            const EdgeInsets.all(
+              CommonDimensions.medium,
+            ),
         decoration: BoxDecoration(
           color: widget.backgroundColor,
           border: widget.border,
@@ -84,21 +124,40 @@ class _ExpansionContainerState extends State<ExpansionContainer>
         ),
         child: Column(
           children: [
-            widget.title,
+            Padding(
+              padding: widget.titlePadding ?? EdgeInsets.zero,
+              child: Row(
+                children: [
+                  widget.title,
+                  if (widget.arrowAlignment == ArrowAlignment.start)
+                    const SizedBox(width: CommonDimensions.large)
+                  else
+                    const Spacer(),
+                  if (widget.showArrow)
+                    AnimatedBuilder(
+                      animation: _rotationAnimation!,
+                      builder: (_, child) {
+                        return Transform.rotate(
+                          angle: _rotationAnimation!.value *
+                              rotationCount *
+                              math.pi,
+                          child: const Icon(Icons.keyboard_arrow_down_outlined),
+                        );
+                      },
+                    ),
+                ],
+              ),
+            ),
             SizeTransition(
-              sizeFactor: _animation,
+              sizeFactor: _expansionAnimation,
               child: Column(
                 children: [
-                  const SizedBox(
-                    height: CommonDimensions.large,
-                  ),
                   if (widget.sizeBetweenChildren != null)
-                    ...widget.children
-                      .insertBetween(
-                        SizedBox(
-                          height: widget.sizeBetweenChildren,
-                        ),
-                      )
+                    ...widget.children.insertBetween(
+                      SizedBox(
+                        height: widget.sizeBetweenChildren,
+                      ),
+                    )
                   else
                     ...widget.children,
                 ],
@@ -110,3 +169,5 @@ class _ExpansionContainerState extends State<ExpansionContainer>
     );
   }
 }
+
+enum ArrowAlignment { start, end }
