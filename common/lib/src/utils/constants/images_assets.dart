fdd8259c018ce727ad96e034cdd612e535ff3a21
softtrack <glebdyakov2000@gmail.com>
2023-07-29 14:15:26 +0300

    Payments history ui: add asset to utils

--- common/lib/src/utils/constants/images_assets.dart
+++ common/lib/src/utils/constants/images_assets.dart
@@ -4,6 +4,7 @@ abstract final class ImagesAssets {
   static const String busIcon = '${_assetsVector}bus_icon.svg';
   static const String swapIcon = '${_assetsVector}swap_icon.svg';
   static const String horizontalSwapIcon = '${_assetsVector}horizontalSwap_icon.svg';
+  static const String emptyPaymentsHistoryIcon = '${_assetsVector}payments_history_empty.svg';
 
   static const String _assetsFolder = 'assets/';
   static const String _assetsVector = '${_assetsFolder}vector/';
