a410f774b2830e519dce731ab204966c75c7ee43
softtrack <glebdyakov2000@gmail.com>
2023-07-29 10:48:00 +0300

    Contacts ui: fix for pipeline

--- avtovas_mobile/lib/src/features/contacts/pages/contacts.dart
+++ avtovas_mobile/lib/src/features/contacts/pages/contacts.dart
@@ -8,7 +8,7 @@ class ContactsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const BaseNavigationPage(
-      body: const ContactsPageBody(),
+      body: ContactsPageBody(),
     );
   }
 }
