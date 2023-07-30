41deec84106b4ef6c8cbbf4ffe762bc734dcedc8
softtrack <glebdyakov2000@gmail.com>
2023-07-30 15:08:25 +0300

    Terms ui web: add constants

---
+++ avtovas_web/lib/src/features/terms/utils/constants/dimensions.dart
@@ -0,0 +1,5 @@
+abstract final class Dimensions {
+  static const double breadcrumbsPaddingBottom = 10;
+  static const double titlePaddingBottom = 25;
+  static const double dropdownPaddingBottom = 41;
+}
