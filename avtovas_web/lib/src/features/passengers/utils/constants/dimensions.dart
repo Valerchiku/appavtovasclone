8dc21df3a252d3f589f05bbf3008834e76aa4aee
softtrack <glebdyakov2000@gmail.com>
2023-07-30 10:46:04 +0300

    Passengers ui: add dimensions for web

---
+++ avtovas_web/lib/src/features/passengers/utils/constants/dimensions.dart
@@ -0,0 +1,9 @@
+abstract final class Dimensions {
+  static const double rootPaddingLeft = 120;
+  static const double mobileRootPaddingLeft = 20;
+  static const double rootPaddingTop = 50;
+  static const double breadcrumbsPaddingBottom = 10;
+  static const double itemsPaddingTop = 25;
+  static const double titleSize = 30;
+  static const double minWebDesktopResolutionWidth = 600;
+}
