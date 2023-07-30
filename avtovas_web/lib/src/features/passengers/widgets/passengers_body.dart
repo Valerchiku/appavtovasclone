79ccff0ec63f0fbd3a672bc670deba3f49f7d059
softtrack <glebdyakov2000@gmail.com>
2023-07-30 10:48:33 +0300

    Passengers ui: add passengers page body for web

---
+++ avtovas_web/lib/src/features/passengers/widgets/passengers_body.dart
@@ -0,0 +1,40 @@
+import 'package:avtovas_web/src/features/passengers/utils/constants/dimensions.dart';
+import 'package:avtovas_web/src/features/passengers/widgets/passengers_item.dart';
+import 'package:common/avtovas_common.dart';
+import 'package:common/src/utils/constants/common_dimensions.dart';
+import 'package:flutter/cupertino.dart';
+
+class PassengersPageBody extends StatelessWidget {
+  const PassengersPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.only(
+        top: Dimensions.rootPaddingTop,
+        left: context.availableWidth > Dimensions.minWebDesktopResolutionWidth ? Dimensions.rootPaddingLeft : Dimensions.mobileRootPaddingLeft,
+      ),
+      children: [
+        Text('Главная / Помощь / Справочная информация'),
+        Padding(
+          padding: EdgeInsets.only(
+            top: Dimensions.breadcrumbsPaddingBottom,
+            bottom: Dimensions.itemsPaddingTop,
+          ),
+          child: Text(
+            context.locale.passengersTitle,
+            style: context.themeData.textTheme.titleLarge?.copyWith(
+              fontSize: Dimensions.titleSize,
+            ),
+          ),
+        ),
+        ...List.generate(5, (index) => PassengersItem(
+          name: 'asd',
+          age: 'asd',
+          gender: 'asd',
+          onSelected: () {},
+        ),),
+      ],
+    );
+  }
+}
