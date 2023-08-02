227748cfa09167ede87b5e75747aac79de3fa341
softtrack <glebdyakov2000@gmail.com>
2023-08-02 09:11:59 +0300

    Passengers ui: fix by review

--- avtovas_mobile/lib/src/features/app/pages/app.dart
+++ avtovas_mobile/lib/src/features/app/pages/app.dart
@@ -103,7 +103,7 @@ class _AppState extends State<App> {
                       localizationsDelegates:
                           AvtovasLocalization.localizationsDelegates,
                       supportedLocales: AvtovasLocalization.supportedLocales,
-                      home: const PassengersScreen(),
+                      home: const Placeholder(),
                       theme: context.themeData,
                     );
                   },
--- avtovas_mobile/lib/src/features/passengers/pages/passengers_screen.dart
+++ avtovas_mobile/lib/src/features/passengers/pages/passengers_screen.dart
@@ -1,5 +1,6 @@
 import 'package:avtovas_mobile/src/common/widgets/base_navigation_page/base_navigation_page.dart';
 import 'package:avtovas_mobile/src/features/passengers/widgets/passengers_screen_body.dart';
+import 'package:common/avtovas_common.dart';
 import 'package:flutter/material.dart';
 
 class PassengersScreen extends StatelessWidget {
@@ -7,8 +8,9 @@ class PassengersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BaseNavigationPage(
-      body: PassengersScreenBody(),
+    return BaseNavigationPage(
+      appBarTitle: context.locale.passengersTitle,
+      body: const PassengersScreenBody(),
     );
   }
 }
