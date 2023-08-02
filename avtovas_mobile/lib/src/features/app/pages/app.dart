3b57884cb5c97e7889b19aa55b575df3194be685
softtrack <glebdyakov2000@gmail.com>
2023-08-02 11:55:04 +0300

    About ui: fix for pipeline

--- avtovas_mobile/lib/src/features/app/pages/app.dart
+++ avtovas_mobile/lib/src/features/app/pages/app.dart
@@ -3,7 +3,6 @@ import 'package:avtovas_mobile/src/common/di/injector.dart';
 import 'package:avtovas_mobile/src/common/shared_cubit/app_overlay/app_overlay_cubit.dart';
 import 'package:avtovas_mobile/src/common/shared_cubit/navigation_panel/navigation_panel_cubit.dart';
 import 'package:avtovas_mobile/src/common/utils/theme_type.dart';
-import 'package:avtovas_mobile/src/features/about/pages/about_page.dart';
 import 'package:avtovas_mobile/src/features/app/cubit/app_cubit.dart';
 import 'package:avtovas_mobile/src/features/main_search/pages/main_search_page.dart';
 import 'package:common/avtovas_common.dart';
