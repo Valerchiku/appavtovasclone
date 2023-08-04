aaced15a9987b91b81172eb6b896664239fb55b6
Tash <tasm86688@gmail.com>
2023-08-04 12:49:53 +0500

    Main models: removed extra model

--- core/lib/domain/entities/trip/trip.dart
+++ core/lib/domain/entities/trip/trip.dart
@@ -109,33 +109,3 @@ final class TripModel extends Equatable {
     this.checkMan,
   });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-class DefaultValueVariant extends Equatable {
-  final String? name;
-  final String? inputMask;
-
-  @override
-  bool? get stringify => true;
-
-  @override
-  List<Object?> get props => [
-        name,
-        inputMask,
-      ];
-
-  const DefaultValueVariant({
-    this.name,
-    this.inputMask,
-  });
-}
