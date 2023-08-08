391b7772cf2a18ee43500cf89942d11052e07795
Tash <tasm86688@gmail.com>
2023-08-08 11:12:49 +0500

    Xml methods: added main methods

---
+++ core/lib/data/utils/xml_methods/xml_methods.dart
@@ -0,0 +1,79 @@
+abstract final class XmlMethods {
+  String getBusStops() {
+    return '''
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sal="http://www.unistation.ru/saleport">
+      <soap:Header/>
+        <soap:Body>
+          <sal:GetBusStops/>
+        </soap:Body>
+    </soap:Envelope>
+  ''';
+  }
+
+  String getDestinations({
+    /// Departure parameter , can be taken from getBusStops
+    required String departure,
+  }) {
+    return '''
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sal="http://www.unistation.ru/saleport">
+      <soap:Header/>
+        <soap:Body>
+          <sal:GetDestinations>
+            <sal:Substring></sal:Substring>
+            <sal:Departure>$departure</sal:Departure>
+          </sal:GetDestinations>
+        </soap:Body>
+    </soap:Envelope>
+  ''';
+  }
+
+  String getTrips({
+    /// Departure parameter , can be taken from getBusStops
+    required String departure,
+
+    /// Destination parameter , can be taken from getBusStops
+    required String destination,
+
+    /// TripsDate parameter must be in the YYYY-MM-DD format
+    required String tripsDate,
+  }) {
+    return '''
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sal="http://www.unistation.ru/saleport">
+      <soap:Header/>
+        <soap:Body>
+          <sal:GetTrips>
+            <sal:Departure>$departure</sal:Departure>
+            <sal:Destination>$destination</sal:Destination>
+            <sal:TripsDate>$tripsDate</sal:TripsDate>
+          </sal:GetTrips>
+        </soap:Body>
+    </soap:Envelope>
+  ''';
+  }
+
+  String getOccupiedSeats({
+    /// TripId parameter , can be taken from getTrips as Id
+    required String tripId,
+
+    /// Departure parameter , can be taken from getTrips
+    required String departure,
+
+    /// Destination parameter , can be taken from getTrips
+    required String destination,
+  }) {
+    return '''
+    <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:sal="http://www.unistation.ru/saleport">
+      <soap:Header/>
+        <soap:Body>
+          <sal:GetOccupiedSeats>
+            <sal:TripId>$tripId</sal:TripId>
+            <sal:Departure>$departure</sal:Departure>
+            <sal:Destination>$destination</sal:Destination>
+            <sal:OrderId></sal:OrderId>
+          </sal:GetOccupiedSeats>
+        </soap:Body>
+      </soap:Envelope>
+  ''';
+  }
+
+}
