ef8e3d14352d6b6646cf51a241da228b9a280204
tokyolem <heenworker@gmail.com>
2023-08-14 11:26:20 +0300

    Search-process-v1: solve analyzer

--- common/lib/src/widgets/searchable_menu/searchable_menu.dart
+++ common/lib/src/widgets/searchable_menu/searchable_menu.dart
@@ -1,8 +1,6 @@
 import 'package:common/avtovas_common.dart';
 import 'package:easy_autocomplete/easy_autocomplete.dart';
-import 'package:field_suggestion/field_suggestion.dart';
 import 'package:flutter/material.dart';
-import 'package:collection/collection.dart';
 
 class SearchableMenu extends StatelessWidget {
   final FocusNode? focusNode;
@@ -72,57 +70,6 @@ class SearchableMenu extends StatelessWidget {
         );
       },
       onChanged: onChanged,
-    );
-  }
-}
-
-final class SuggestionField extends StatelessWidget {
-  final TextEditingController controller;
-  final List<Widget> suggestions;
-  final bool Function(Widget, String) onSearch;
-  final String hintText;
-
-  const SuggestionField({
-    required this.controller,
-    required this.suggestions,
-    required this.onSearch,
-    required this.hintText,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final colorPath = context.theme;
-    final themeStyle = context.themeData.textTheme.headlineMedium!.copyWith(
-      fontWeight: CommonFonts.weightRegular,
-      color: colorPath.secondaryTextColor,
-    );
-
-    return FieldSuggestion(
-      itemBuilder: (_, index) => suggestions[index],
-      textController: controller,
-      suggestions: suggestions,
-      search: onSearch,
-      inputDecoration: InputDecoration(
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: CommonDimensions.large,
-          vertical: CommonDimensions.medium,
-        ),
-        filled: true,
-        fillColor: colorPath.containerBackgroundColor,
-        border: InputBorder.none,
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: colorPath.containerBackgroundColor,
-          ),
-        ),
-        hintText: hintText,
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: colorPath.containerBackgroundColor,
-          ),
-        ),
-      ),
     );
   }
 }
