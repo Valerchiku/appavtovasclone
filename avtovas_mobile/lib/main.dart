47a522858f9e73599068e16167f8fa9bc1574859
tokyolem <heenworker@gmail.com>
2023-08-15 19:52:58 +0300

    Authorization data: add authorization repository, interactor, callbacks, etc.

--- avtovas_mobile/lib/main.dart
+++ avtovas_mobile/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:avtovas_mobile/src/features/app/pages/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   injectDependencies();
 
   runApp(const App());
