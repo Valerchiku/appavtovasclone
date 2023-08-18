ae2815f201d588e9e5c2de438a27cff5d8de5905
Tash <tasm86688@gmail.com>
2023-08-18 19:19:11 +0500

    Get trip request: fixed analyze issue

--- avtovas_mobile/lib/src/features/trips_schedule_page/cubit/trips_schedule_cubit.dart
+++ avtovas_mobile/lib/src/features/trips_schedule_page/cubit/trips_schedule_cubit.dart
@@ -5,7 +5,6 @@ import 'package:avtovas_mobile/src/common/utils/sort_options.dart';
 import 'package:avtovas_mobile/src/common/widgets/base_navigation_page/utils/route_helper.dart';
 import 'package:common/avtovas_common.dart';
 import 'package:common/avtovas_navigation.dart';
-import 'package:core/avtovas_core.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
