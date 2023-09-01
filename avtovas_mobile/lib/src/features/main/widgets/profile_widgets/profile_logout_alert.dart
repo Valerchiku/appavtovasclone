669bc27ba200947dec7717b94c8a2906faacf40f
glebDyakov <glebdyakov2000@gmail.com>
2023-09-01 21:16:18 +0300

    Logout alert dialog: add profile logout alert

---
+++ avtovas_mobile/lib/src/features/main/widgets/profile_widgets/profile_logout_alert.dart
@@ -0,0 +1,39 @@
+import 'package:avtovas_mobile/src/common/shared_cubit/navigation_panel/navigation_panel_cubit.dart';
+import 'package:avtovas_mobile/src/features/authorization/cubit/authorization_cubit.dart';
+import 'package:common/avtovas_common.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+final class ProfileLogoutAlert extends StatefulWidget {
+  final NavigationPanelCubit cubit;
+
+  const ProfileLogoutAlert({
+    required this.cubit,
+    super.key,
+  });
+
+  @override
+  State<ProfileLogoutAlert> createState() => _ProfileLogoutAlertState();
+}
+
+class _ProfileLogoutAlertState extends State<ProfileLogoutAlert> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<NavigationPanelCubit, NavigationPanelState>(
+      bloc: widget.cubit,
+      builder: (context, state) {
+        return Container(
+          width: context.availableWidth,
+          height: context.availableHeight,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 0, 0, 0.7),
+          ),
+          child: AvtovasAlertDialog(
+            title: context.locale.exitWarning,
+            okayCallback: widget.cubit.toggleLogoutAlert,
+          ),
+        );
+      },
+    );
+  }
+}
