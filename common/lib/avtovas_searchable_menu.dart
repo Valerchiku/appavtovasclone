117f3fc960712438885086190827843389aea114
Muhammed Tash <119746836+M26Tash@users.noreply.github.com>
2023-09-18 11:20:18 +0300

    Merge 'feature/separate-common-core-exports' to 'dev' (#94)

---
+++ common/lib/avtovas_searchable_menu.dart
@@ -0,0 +1,4 @@
+export 'package:common/src/widgets/searchable_menu/easy_auto_complete.dart';
+export 'package:common/src/widgets/searchable_menu/filterable_list.dart';
+export 'package:common/src/widgets/searchable_menu/searchable_menu.dart';
+export 'package:common/src/widgets/searchable_menu/searchable_menu_suggestion_item.dart';
